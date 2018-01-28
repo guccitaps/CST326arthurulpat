b641d7600a6b387b0deb5ca589eace4603ad7964
guccitaps <arthur.ulpat@epita.fr>
2018-01-27 20:58:30 -0800

    activity 1A

---
+++ Activity 1A/Assets/MyFirstBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstBehaviour : MonoBehaviour {
+	public int repeatNo = 10;
+	public int count = 500;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(transform.position);
+		if (count % 50 == 0) {
+			Debug.Log (repeatNo);
+			repeatNo--;
+		}
+		if (count > 1) {
+			count--;
+		}
+	}
+}
